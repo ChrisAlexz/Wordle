e050df5b47b00863bde16b5d55357553b1d31ba5
ChrisAlexz <chrishern869@gmail.com>
2025-01-21 22:35:55 -0600

    defining states

--- React-Wordle/src/App.js
+++ React-Wordle/src/App.js
@@ -1,9 +1,24 @@
+import { useEffect,useState } from "react";
 
 
 function App() {
+  const [solution,setSolution] = useState(null)
+  // fire a function when component first renders, the [] is so that it only fires once
+  useEffect(()=> {
+    fetch('http://localhost:3001/solutions')
+    .then(res => res.json()) // turns response from JSON file to JS(parsed)
+    //json represents the parsed data now from json to js
+    .then(json => {
+      //random int between 0-14
+      const randomSolution = json[Math.floor(Math.random()*json.length)]
+      setSolution(randomSolution.word)
+    })
+  },[setSolution])
+  //^ is dependency array ([setSolution]), meaning it will run anytime setSolution changes
   return (
     <div className="App">
       <h1>Wordle (Lingo)</h1>
+      {solution && <div>solution is: {solution}</div>}
     </div>
   );
 }
