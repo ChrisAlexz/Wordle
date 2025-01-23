e4688ea3fef5a9bbfa59be074a83749ccbedfdf5
ChrisAlexz <chrishern869@gmail.com>
2025-01-22 19:41:09 -0600

    Submiting

--- React-Wordle/src/hooks/useWordle.js
+++ React-Wordle/src/hooks/useWordle.js
@@ -10,8 +10,9 @@ const useWordle = (solution) => {
     // format a guess into an array of letter objects
     // e.g. [{key: 'a', color: 'yellow'}]
     // once word submitted
-    const formatGuess = () => {
 
+    const formatGuess = () => {
+        console.log("formating the guess - ", currentGuess)
     }
     // add a new guess to the guesses state
     // update the isCorrect state if the guess is correct
@@ -26,6 +27,26 @@ const useWordle = (solution) => {
     // every time a user types
     const handleKeyup = ({key}) => { // key is destructured from keyup eventListener
         // only if a user types a letter key and nothin else
+        if (key === 'Enter'){
+            // only add guess if turn is less than 5
+            if (turn > 5)
+            {
+                console.log("You used all your guesses")
+                return 
+            }
+            // do not allow duplicate words // if hist includes the current guess // true or false
+            if (history.includes(currentGuess)) {
+                console.log("You already tried that word")
+                return
+            }
+            // check word is 5 char long
+            if(currentGuess.length !== 5) {
+                console.log("Word must be 5 characters long!")
+                return
+            }
+            // only call this function if all other conditions are bypassed
+            formatGuess()
+        }
         if(key === "Backspace") {
             setCurrentGuess((prev) => {
                 return prev.slice(0,-1) // basically if backspace clicked it will remove a char
