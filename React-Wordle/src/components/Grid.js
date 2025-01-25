b588642b0781b9f9872819f50e7a49676b3167f1
ChrisAlexz <chrishern869@gmail.com>
2025-01-24 22:59:03 -0600

    Current guess

--- React-Wordle/src/components/Grid.js
+++ React-Wordle/src/components/Grid.js
@@ -4,8 +4,13 @@ import Row from './Row'
 export default function Grid({currentGuess, guesses, turn}) { //props
   return (
     <div>
-        {guesses.map((g,i)=> {
-            return <Row key={i}/>
+        {guesses.map((g,i)=> { 
+          if(turn === i) {// second thing they see after they start typing
+            return <Row key={i} currentGuess={currentGuess}/>
+
+          }
+            // iterates through guesses array(5) and prints them // first thing they see
+            return <Row key={i} guess={g}/>
         })}
     </div>
   )
